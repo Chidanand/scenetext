4620d156f1fd49c200fba2b3356501dc62b0e012
tbrsun <tbrsun@76f417cf-82ff-fdd3-a480-e36434aa4511>
2012-06-11 07:26:41 +0000

    gogo

--- construct_collision.m
+++ construct_collision.m
@@ -1,16 +1,79 @@
-function A = construct_collision(bbs)
+function Out = construct_collision(I,bbs,bigram_prob)
 n = size(bbs,1);
-A = zeros(n,n);
 
+% First feature (collision)
+A = zeros(n,n);
 for i=1:n
     for j=i+1:n
         bb1 = bbs(i,:);
         bb2 = bbs(j,:);
         inters = bbApply( 'intersect', bb1, bb2 );
-        uin = bbApply( 'union', bb1, bb2 );
-        A(i,j) = bbApply( 'area', inters ) / bbApply( 'area', uin );
+        A(i,j) = bbApply( 'area', inters );
+%         uin = bbApply( 'union', bb1, bb2 );
+%         A(i,j) = bbApply( 'area', inters ) / bbApply( 'area', uin );
     end
 end
-
 A = A + A';
 A = A / max(A(:));
+
+% Second feature (collision)
+B = zeros(n,n);
+for i=1:n
+    for j=1:n
+        if (i==j)
+            continue;
+        end
+        bb1 = bbs(i,6);
+        bb2 = bbs(j,6);
+        B(i,j) = bigram_prob(bb1,bb2);
+    end
+end
+B = B / max(B(:));
+
+% % Second feature (distance)
+% B = zeros(n,n);
+% for i=1:n
+%     for j=i+1:n
+%         bb1 = bbs(i,:);
+%         bb2 = bbs(j,:);
+%         cen1 = [bb1(1,1)+0.5*bb1(1,3) bb1(1,2)+0.5*bb1(1,4)];
+%         cen2 = [bb2(1,1)+0.5*bb2(1,3) bb2(1,2)+0.5*bb2(1,4)];
+%         
+%         dist = norm(cen1-cen2,2);
+%         B(i,j) = dist;
+%     end
+% end
+% B = B + B';
+% B = B / max(B(:));
+
+% Thrid feature (color similarity)
+C = zeros(n,n);
+nbins = 15;
+for i=1:n
+    for j=i+1:n
+        close all;
+        bb1 = bbs(i,:);
+        bb2 = bbs(j,:);
+        I1 = I(bb1(1,2):bb1(1,2)+bb1(1,4)-1,bb1(1,1):bb1(1,1)+bb1(1,3)-1,:);
+        I2 = I(bb2(1,2):bb2(1,2)+bb2(1,4)-1,bb2(1,1):bb2(1,1)+bb2(1,3)-1,:);
+%         figure;imshow(I1);figure;imshow(I2);
+        hist_1 = [];
+        hist_2 = [];
+        for k=1:3 % for each channel
+            [n1,y] = imhist(I1(:,:,k),nbins);
+            hist_1 = [hist_1; n1/max(n1)];
+            [n2,y] = imhist(I2(:,:,k),nbins);
+            hist_2 = [hist_2; n2/max(n2)];
+        end
+        vec1 = hist_1 / sum(hist_1);
+        vec2 = hist_2 / sum(hist_2);
+        dist = 0.5*sum( (vec1 - vec2).^2 ./ (vec1 + vec2 + eps) );
+        C(i,j) = dist;
+    end
+end
+C = C + C';
+C = C / max(C(:));
+
+% Out = A;
+Out = A - B + C;
+%Out = Out / max(Out(:));
--- wordSpot.m
+++ wordSpot.m
@@ -41,20 +41,20 @@ vary_size = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1];    % steps for varying 
 lex=wordDet('build',lexS);
 % % run character detector (Ferns)
 t1S=tic; 
-    % varying sizes
-    bbs = [];
-    for q=1:length(vary_size)
-        r = 1/vary_size(q);
-        I2=imResample(I,[round(size(I,1)),round(size(I,2)*r)]);
-        bbs_q=charDet(I2,fModel,frnPrms);
-        bbs_q(:,[1 3])=bbs_q(:,[1 3])/r;
-        bbs = [bbs; bbs_q];
-    end
+%     % varying sizes
+%     bbs = [];
+%     for q=1:length(vary_size)
+%         r = 1/vary_size(q);
+%         I2=imResample(I,[round(size(I,1)),round(size(I,2)*r)]);
+%         bbs_q=charDet(I2,fModel,frnPrms);
+%         bbs_q(:,[1 3])=bbs_q(:,[1 3])/r;
+%         bbs = [bbs; bbs_q];
+%     end
 t1=toc(t1S);
 
-% f_name=fullfile(sprintf('data/BB_I%05i.mat',ff));
+f_name=fullfile(sprintf('data/BB_I%05i.mat',ff));
 % save(f_name,'bbs');
-% load(f_name);
+load(f_name);
 
 % prunning by aspect ratio (case sensitive)
 bbs=asp_prun(bbs,ap_mean,ap_var,ch);
@@ -64,19 +64,18 @@ bbs(:,6)=equivClass(bbs(:,6),ch);
 nmsPrms{8} = {1,1};
 bbs=bbNms(bbs,nmsPrms);
 
-% f_name=fullfile(sprintf('data/bbs/BB_I%05i.mat',ff));
-% save(f_name,'bbs');
-% load(f_name);
 
-% [val idx] = sort(bbs(:,5),'descend');
+[val idx] = sort(bbs(:,5),'descend');
 % bbs = bbs(idx(1:20),:);
-% imshow(I);charDetDraw(bbs(idx(1:10),:),ch);
+imshow(I);charDetDraw(bbs(idx(1:20),:),ch);
 
-% A = construct_collision(bbs);
-% s = bbs(:,5);
-% v = convex_magic(A,s);
-% imshow(I);charDetDraw(bbs(v==1,:),ch);
-% bbs = bbs(v==1,:);
+load bigram_prob2;
+bbs = bbs(idx(1:20),:);
+A = construct_collision(I,bbs,bigram_prob);
+s = bbs(:,5);
+v = convex_magic(A,s);
+imshow(I);charDetDraw(bbs(v==1,:),ch);
+bbs = bbs(v==1,:);
 
 bbs(:,5) = bbs(:,5)*500;
 
